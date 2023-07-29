be8a962da8fd38d76265d0ed71593e03ed5e211f
Emirhan <125173937+DeVBulut@users.noreply.github.com>
2023-07-28 22:30:03 -0600

    File Setup

---
+++ JARVIS Virtual Assistant/JARVIS Virtual Assistant/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JARVIS_Virtual_Assistant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello I am Jarvis");
+            Console.WriteLine(Console.WindowWidth);
+            Console.SetWindowSize(5000, 50);
+            Console.WriteLine(Console.WindowWidth);
+            Console.ReadLine();
+            Console.SetWindowSize(50, 50);
+        }
+    }
+}
