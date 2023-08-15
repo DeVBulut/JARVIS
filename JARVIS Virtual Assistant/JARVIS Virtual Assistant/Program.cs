ddea7c105b3ede2aff07cec384de1d9bf5c546e0
Emirhan <125173937+DeVBulut@users.noreply.github.com>
2023-08-15 12:08:44 -0600

    LocalTime Coded
    in comment sings

--- JARVIS Virtual Assistant/JARVIS Virtual Assistant/Program.cs
+++ JARVIS Virtual Assistant/JARVIS Virtual Assistant/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace JARVIS_Virtual_Assistant
 {
@@ -17,6 +18,16 @@ namespace JARVIS_Virtual_Assistant
             Console.WriteLine("\r\n   `7MMF'    db      `7MM\"\"\"Mq.`7MMF'   `7MF'`7MMF' .M\"\"\"bgd \r\n     MM     ;MM:       MM   `MM. `MA     ,V    MM  ,MI    \"Y \r\n     MM    ,V^MM.      MM   ,M9   VM:   ,V     MM  `MMb.     \r\n     MM   ,M  `MM      MMmmdM9     MM.  M'     MM    `YMMNq. \r\n     MM   AbmmmqMA     MM  YM.     `MM A'      MM  .     `MM \r\n(O)  MM  A'     VML    MM   `Mb.    :MM;       MM  Mb     dM \r\n Ymmm9 .AMA.   .AMMA..JMML. .JMM.    VF      .JMML.P\"Ybmmd\" \r\n ");
             Console.WriteLine("──────────────────────────────────────────────────────────────");
             //Execute Login Sequence
+            //TimeZone localZone = TimeZone.CurrentTimeZone;
+            //DateTime currentDate = DateTime.Now;
+            //DispatcherTimer timer = new DispatcherTimer();
+            //timer.Interval = new TimeSpan(0, 5, 0); // sets it to 5 minutes
+            //timer.Tick += new EventHandler(timer_Tick);
+            //timer.Start();
+            //if (currentDate.Hour == 11 && currentDate.Minute == 29)
+            //{
+            //    Console.WriteLine("Ello");
+            //}
             try { loginManager.Intro(); }
             catch (Exception e) { if (loginManager.logType == 2) { Console.WriteLine("$$$ Error Code : " + e); } }
             Console.ReadLine();
