b7181a0c5fdc93352caffaa026a0dc4678d00146
Emirhan <125173937+DeVBulut@users.noreply.github.com>
2023-09-19 16:37:10 -0600

    idk what I changed

--- JARVIS Virtual Assistant/JARVIS Virtual Assistant/Program.cs
+++ JARVIS Virtual Assistant/JARVIS Virtual Assistant/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Threading;
 
@@ -17,22 +18,20 @@ namespace JARVIS_Virtual_Assistant
             Console.WriteLine("──────────────────────────────────────────────────────────────");
             Console.WriteLine("\r\n   `7MMF'    db      `7MM\"\"\"Mq.`7MMF'   `7MF'`7MMF' .M\"\"\"bgd \r\n     MM     ;MM:       MM   `MM. `MA     ,V    MM  ,MI    \"Y \r\n     MM    ,V^MM.      MM   ,M9   VM:   ,V     MM  `MMb.     \r\n     MM   ,M  `MM      MMmmdM9     MM.  M'     MM    `YMMNq. \r\n     MM   AbmmmqMA     MM  YM.     `MM A'      MM  .     `MM \r\n(O)  MM  A'     VML    MM   `Mb.    :MM;       MM  Mb     dM \r\n Ymmm9 .AMA.   .AMMA..JMML. .JMM.    VF      .JMML.P\"Ybmmd\" \r\n ");
             Console.WriteLine("──────────────────────────────────────────────────────────────");
-            //Execute Login Sequence
-            //TimeZone localZone = TimeZone.CurrentTimeZone;
-            //DateTime currentDate = DateTime.Now;
-            //DispatcherTimer timer = new DispatcherTimer();
-            //timer.Interval = new TimeSpan(0, 5, 0); // sets it to 5 minutes
-            //timer.Tick += new EventHandler(timer_Tick);
-            //timer.Start();
-            //if (currentDate.Hour == 11 && currentDate.Minute == 29)
-            //{
-            //    Console.WriteLine("Ello");
-            //}
+           
             try { loginManager.Intro();
-                ListDesigner _listDesigner = new ListDesigner();
-               // _listDesigner.createList("tabletable", "column1", "int");
-               AnimationAsisstant _animAssistant = new AnimationAsisstant();
-                _animAssistant.LoadAnimation(15);
+                TimeLib _timeLib = new TimeLib();
+                Console.WriteLine("enter hour");
+                string hourEntry = Console.ReadLine();
+                Console.WriteLine("enter minute");
+                string minuteEntry = Console.ReadLine();
+                int parsedHourEntry = Int32.Parse(hourEntry);
+                int parsedMinuteEntry = Int32.Parse(minuteEntry);
+                Console.WriteLine(parsedHourEntry);
+                Console.WriteLine(parsedMinuteEntry);
+
+                Console.WriteLine(_timeLib.setAlarm(parsedHourEntry, parsedMinuteEntry));
+                //_timeLib.getTime();
             }
             catch (Exception e) { if (loginManager.logType == 2) { Console.WriteLine("$$$ Error Code : " + e); } }
             Console.ReadLine();
---
+++ JARVIS Virtual Assistant/JARVIS Virtual Assistant/TimeLib.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace JARVIS_Virtual_Assistant
+{
+    internal class TimeLib
+    {
+        public void getTime()
+        {
+            TimeZone localZone = TimeZone.CurrentTimeZone;
+            DateTime currentDate = DateTime.Now;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 15); // sets it to 5 minutes
+            timer.Start();
+            if (currentDate.Hour == 9 && currentDate.Minute == 30)
+            {
+                Console.WriteLine("Ello");
+            }
+        }
+
+        public string setAlarm(int hour, int minute)
+        {
+            DateTime currentDate = DateTime.Now;
+            while (true) {
+                currentDate = DateTime.Now;
+                if (currentDate.Hour == hour && currentDate.Minute == minute)
+                {
+                    return "Alarm Executing";
+                }
+            }
+        }
+
+        
+    }
+}
